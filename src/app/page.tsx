0a1483872a44800e94d01a41907de1026e7ae374
Dagadev <gacub20ru@gmail.com>
2024-04-12 22:26:30 -0600

    Remove .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+'use client'
 import styles from './page.module.css'
+import { useState } from 'react'
+
+interface Password {
+  id: string
+  has: string
+  name: string
+}
 
 export default function Home () {
+  const [passwords, setPasswords] = useState<Password[]>(() => {
+    const passStr = localStorage.getItem('passwords')
+
+    if (passStr === null) return []
+
+    return JSON.parse(passStr)
+  })
+
   return (
     <main className={styles.main}>
       <h1>Crypto password</h1>
+
+      <ul>
+        {passwords.map(p => <li key={p.id}>
+          <strong>{p.name}</strong>
+          <p>{p.has}</p>
+        </li>)}
+      </ul>
     </main>
   )
 }
