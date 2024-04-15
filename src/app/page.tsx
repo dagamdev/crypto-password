e1cd0c842b2e66de37d99f6621b029d933b54df1
Dagadev <gacub20ru@gmail.com>
2024-04-15 17:46:35 -0600

    Secondari changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,73 @@
 'use client'
-import styles from './page.module.css'
-import { useState } from 'react'
-
-interface Password {
-  id: string
-  has: string
-  name: string
-}
+import styles from '@/styles/page.module.css'
+import { useEffect, type FormEvent } from 'react'
+import requestService from '@/services/request'
+import Password from '@/components/password'
+import { usePasswordsStore } from '@/providers/passwords-provider'
 
 export default function Home () {
-  const [passwords, setPasswords] = useState<Password[]>(() => {
+  const { passwords, setPasswords } = usePasswordsStore(s => s)
+
+  useEffect(() => {
+    if (typeof localStorage === 'undefined') return
+
     const passStr = localStorage.getItem('passwords')
 
-    if (passStr === null) return []
+    if (passStr === null) return
 
-    return JSON.parse(passStr)
-  })
+    setPasswords(JSON.parse(passStr))
+  }, [])
+
+  const handleSubmit = async (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault()
+
+    const name = ev.currentTarget.passName.value
+    const password = ev.currentTarget.password.value
+
+    ev.currentTarget.passName.value = ''
+    ev.currentTarget.password.value = ''
+
+    try {
+      const data = await requestService.encrypt<Record<'iv' | 'encrypted', string>>(password)
+
+      if (typeof localStorage === 'undefined') return
+
+      const updatedPasswords = [...passwords, {
+        id: crypto.randomUUID(),
+        name,
+        key: data.iv,
+        hash: data.encrypted
+      }]
+
+      setPasswords(updatedPasswords)
+      localStorage.setItem('passwords', JSON.stringify(updatedPasswords))
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <main className={styles.main}>
       <h1>Crypto password</h1>
 
-      <ul>
-        {passwords.map(p => <li key={p.id}>
-          <strong>{p.name}</strong>
-          <p>{p.has}</p>
-        </li>)}
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <h2>Create password</h2>
+
+        <label>
+          Name
+          <input name='passName' type="text" required />
+        </label>
+        <label>
+          Password
+          <input name='password' type="password" required />
+        </label>
+
+        <button>Add password</button>
+      </form>
+
+      {passwords.length !== 0 && <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {passwords.map(p => <Password key={p.id} password={p} />)}
+      </ul>}
     </main>
   )
 }
