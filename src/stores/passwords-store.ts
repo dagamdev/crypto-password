8aa9ec9822d89bc6aec9294ba9ebf27702d87675
Dagadev <gacub20ru@gmail.com>
2024-04-16 15:55:49 -0600

    Update passwords store & Add persist middleware

--- src/stores/passwords-store.ts
+++ src/stores/passwords-store.ts
@@ -1,28 +1,35 @@
-import { createStore } from 'zustand/vanilla'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export interface PasswordsState {
   passwords: Password[]
 }
 
 export interface PasswordsActions {
-  addPassword: (newPassword: Password) => void
+  addPassword: (newPassword: Omit<Password, 'id'>) => void
   removePassword: (passwordId: string) => void
   setPasswords: (passwords: Password[]) => void
+  updatePassword: (passwordId: string, newData: Partial<Omit<Password, 'id'>>) => void
 }
 
 export type PasswordsStore = PasswordsState & PasswordsActions
 
-export const createPasswordsStore = (initState: PasswordsState = { passwords: [] }) => {
-  return createStore<PasswordsStore>()((set) => ({
-    ...initState,
+export const usePasswordsStore = create<PasswordsStore>()(
+  persist((set, get) => ({
+    passwords: [],
     addPassword (newPassword) {
-      set((state) => ({ passwords: [...state.passwords, newPassword] }))
+      set((state) => ({ passwords: [...state.passwords, { id: crypto.randomUUID(), ...newPassword }] }))
     },
     removePassword (passwordId) {
       set(state => ({ passwords: state.passwords.filter(p => p.id !== passwordId) }))
     },
     setPasswords (passwords) {
-      set(() => ({ passwords }))
+      set({ passwords })
+    },
+    updatePassword (passwordId, newData) {
+      set((state) => ({ passwords: state.passwords.map(p => p.id === passwordId ? { ...p, ...newData } : p) }))
     }
-  }))
-}
+  }), {
+    name: 'passwords'
+  })
+)
