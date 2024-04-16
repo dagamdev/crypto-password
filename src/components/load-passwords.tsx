7c57b606c2856f8ae2b8a76244294312f8b5c285
Dagadev <gacub20ru@gmail.com>
2024-04-16 15:50:19 -0600

    Add LoadPassword component

---
+++ src/components/load-passwords.tsx
@@ -0,0 +1,8 @@
+export default function LoadPasswords () {
+  return (
+    <label>
+      Load backup passwords
+      <input type="file" accept=".json" id="backupPasswordsInput" />
+    </label>
+  )
+}
