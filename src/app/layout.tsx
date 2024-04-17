0231c342ebf39f626cb7ffff16005a7b455db948
Dagadev <gacub20ru@gmail.com>
2024-04-16 19:19:52 -0600

    Add soner notifications & Update get project version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
+import packageData from '../../package.json'
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,15 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toaster richColors position='top-center' theme='system' pauseWhenPageIsHidden offset={16} toastOptions={{
+          className: 'toast'
+        }} />
+        {children}
+        <div style={{ textAlign: 'center' }}>
+          <span>v{packageData.version}</span>
+        </div>
+      </body>
     </html>
   )
 }
