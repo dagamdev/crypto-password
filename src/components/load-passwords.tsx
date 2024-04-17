117e2f0acd71e4233392ce4e4c7bbef2f4d21468
Dagadev <gacub20ru@gmail.com>
2024-04-17 09:41:39 -0600

    Add load password file

--- src/components/load-passwords.tsx
+++ src/components/load-passwords.tsx
@@ -1,8 +1,33 @@
+import { usePasswordsStore } from '@/stores/passwords-store'
+import { type ChangeEvent } from 'react'
+import { toast } from 'sonner'
+
 export default function LoadPasswords () {
+  const setPasswords = usePasswordsStore(store => store.setPasswords)
+
+  const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    const file = ev.target.files?.[0]
+    const reader = new FileReader()
+
+    if (file === undefined) return
+
+    reader.onload = (ev) => {
+      const content = ev.target?.result
+
+      if (typeof content !== 'string') return
+
+      setPasswords(JSON.parse(content))
+      toast.success('Passwords uploaded by backup file')
+    }
+
+    reader.readAsText(file)
+    ev.target.value = ''
+  }
+
   return (
     <label>
       Load backup passwords
-      <input type="file" accept=".json" id="backupPasswordsInput" />
+      <input onChange={handleChange} type="file" accept=".json" id="backupPasswordsInput" />
     </label>
   )
 }
