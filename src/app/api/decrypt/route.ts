e88c4bf68a261d953bead43dfaabb174f4774e12
Dagadev <gacub20ru@gmail.com>
2024-04-18 14:47:03 -0600

    Use files and ignore query params & create decryptObject function & Add decrypt objects in array

--- src/app/api/decrypt/route.ts
+++ src/app/api/decrypt/route.ts
@@ -1,24 +1,87 @@
+import { type NextRequest } from 'next/server'
 import { decrypt } from '../utils/crypto'
 
-export async function POST (req: Request) {
+export async function POST (req: NextRequest) {
   try {
-    const { hash, key } = await req.json()
+    const body = await req.json()
+    const decryptedData: Record<string, string> | Array<Record<string, string>> = {}
+    const searchParams = req.nextUrl.searchParams
+    const files = searchParams.get('files')?.split(',')
+    const ignore = searchParams.get('ignore')?.split(',')
+    // console.log({ files, ignore, body })
 
-    if (hash === undefined) {
-      return Response.json({
-        error: 'hash is undefined'
-      })
+    const decryptObject = <T extends Record<string, string>> (obj: T) => {
+      for (const key in obj) {
+        const text = obj[key]
+
+        if (typeof text !== 'string') {
+          return Response.json({
+            error: `${key} is not of type string`
+          })
+        }
+
+        const [encrypted, iv] = text.split('-')
+
+        if (encrypted === undefined && iv === undefined) {
+          return Response.json({
+            error: `The text of the ${key} parameter to be decrypted is invalid`
+          })
+        }
+
+        const setDecryptedValue = () => {
+          obj[key] = decrypt(encrypted, iv) as T[Extract<keyof T, string>]
+        }
+
+        if (files !== undefined) {
+          if (files.includes(key)) setDecryptedValue()
+        } else if (ignore !== undefined) {
+          if (!ignore.includes(key)) setDecryptedValue()
+        } else {
+          setDecryptedValue()
+        }
+      }
+
+      return obj
     }
 
-    if (key === undefined) {
-      return Response.json({
-        error: 'key is undefined'
-      })
+    if (Array.isArray(body)) {
+      const decryptedList = []
+
+      for (const obj of body) {
+        const decryptedObj = decryptObject(obj)
+
+        if (decryptObject instanceof Response) {
+          return decryptObject
+        }
+
+        decryptedList.push(decryptedObj)
+      }
+
+      return Response.json(decryptedList)
+    } else {
+      const decryptedObj = decryptObject(body)
+      for (const key in body) {
+        const text = body[key]
+
+        if (typeof text !== 'string') {
+          return Response.json({
+            error: `${key} is not of type string`
+          })
+        }
+
+        const [encrypted, iv] = text.split('-')
+
+        if (encrypted === undefined && iv === undefined) {
+          return Response.json({
+            error: `The text of the ${key} parameter to be decrypted is invalid`
+          })
+        }
+
+        decryptedData[key] = decrypt(encrypted, iv)
+      }
+
+      return Response.json(decryptedObj)
     }
-
-    const decrypted = decrypt(hash, key)
-
-    return Response.json(decrypted)
   } catch (error) {
     return Response.json({
       error: error instanceof Error ? error.message : 'An error has ocurred'
