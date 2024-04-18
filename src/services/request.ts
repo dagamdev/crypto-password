a0f4081807e2d20ab1868a1edcaadf4de59ea247
Dagadev <gacub20ru@gmail.com>
2024-04-18 14:43:44 -0600

    Update encrypt and decrypt functions

--- src/services/request.ts
+++ src/services/request.ts
@@ -6,13 +6,19 @@ const api = axios.create({
   baseURL: apiUrl
 })
 
-async function encrypt <T extends Record<string, string>> (data: T) {
+type BaseData = Record<string, string> | Array<Record<string, string>> | any
+
+async function encrypt <T extends BaseData> (data: T) {
   const res = await api.post<T>('encrypt', data)
   return res.data
 }
 
-async function decrypt <T extends Record<string, string>> (data: T) {
-  const res = await api.post<T>('decrypt', data)
+async function decrypt <T extends BaseData> (data: T, queries?: {
+  files?: string
+  ignore?: string
+}) {
+  const queriesStr = queries === undefined ? '' : '?' + new URLSearchParams(queries).toString()
+  const res = await api.post<T>(`decrypt${queriesStr}`, data)
   return res.data
 }
 
