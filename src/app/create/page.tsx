747f65232fb5868e41de2c07ba38e508bddc1f75
Dagadev <gacub20ru@gmail.com>
2024-06-06 21:08:37 -0600

    Add create page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Create () {
+  return (
+    <>
+      <Link href='/create/password'>Create password</Link>
+      <Link href='/create/service'>Create service</Link>
+    </>
+  )
+}
