4ed2e4bf6c69e64009d0ab54525e9d1dbc1d32f9
Dagadev <gacub20ru@gmail.com>
2024-06-06 21:09:57 -0600

    Replace h1 by anchor

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import LoadPasswords from './load-passwords'
 export default function Header () {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Crypto password</h1>
+      <a className={styles.title} href='/'>Crypto password</a>
       <LoadPasswords />
     </header>
   )
