b2641d8a6752c339f8da420314ee219415a67e89
Dagadev <gacub20ru@gmail.com>
2024-06-06 21:08:56 -0600

    Add create password pages

---
+++ src/app/create/password/page.tsx
@@ -0,0 +1,10 @@
+import AddPassword from '@/components/add-password'
+
+export default function CreatePassword () {
+  return (
+    <main>
+      Create password
+      <AddPassword />
+    </main>
+  )
+}
